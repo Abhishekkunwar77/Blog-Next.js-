37fb6091924db9f4cd59c11b23ce32faac81b3db
abhishek <aailabeu@gmail.com>
2025-03-15 20:56:51 +0530

    Day1(2025-03-15)

---
+++ app/page.js
@@ -0,0 +1,11 @@
+'use client'
+import BlogItem from "@/Components/BlogItem";
+import  Header from "@/Components/Header"
+export default function Home() {
+  return (
+   <>
+<Header/>
+<BlogItem/>
+    </>
+  );
+}
